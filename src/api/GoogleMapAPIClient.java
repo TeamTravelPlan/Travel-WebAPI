132527993b768088ec6a3350c762fb919f14b0f7
MShiii <meng.shi8@gmail.com>
2019-12-17 09:08:06 -0800

    Add skeleton API methods.

--- src/api/GoogleMapAPIClient.java
+++ src/api/GoogleMapAPIClient.java
@@ -9,11 +9,20 @@ public class GoogleMapAPIClient {
 
     // TODO: findPlace
     // https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=New%20York&fields=photos,formatted_address,name,rating,opening_hours,geometry&inputtype=textquery&key=AIzaSyA-xhthFchWNRXSkamJ8i2SzoEpTl3Fd1M
+    public void findPlace() {
+        return;
+    }
 
     // TODO: getNearbyPlaces
     // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&types=tourist_attraction&keyword=cruise&key=AIzaSyA-xhthFchWNRXSkamJ8i2SzoEpTl3Fd1M
+    public void getNearbyPlaces() {
+        return;
+    }
 
     // TODO: getPlaceDetails
     // https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating,formatted_phone_number&key=YOUR_API_KEY
     // https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyA-xhthFchWNRXSkamJ8i2SzoEpTl3Fd1M
+    public void getPlaceDetails() {
+        return;
+    }
 }
